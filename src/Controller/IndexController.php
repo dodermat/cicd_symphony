2cb04a80e4291ad4fa421b3647c491d780a9119f
Daniel Odermatt <daniel.odermatt@hsr.ch>
2020-02-24 15:01:19 +0100

    test build

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,7 +13,7 @@ class IndexController extends AbstractController
     public function index()
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'Daniel',
+            'controller_name' => 'IndexController',
         ]);
     }
 }
